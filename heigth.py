e145ad7d3a2be7afe8eaa2ea15c98e7957ee46bc
AdrianCristianSantos <adrian.cristian.st@gmail.com>
2020-11-16 13:07:49 -0300

    agora o heigth e o read_txt funcionam perfeitamente

--- heigth.py
+++ heigth.py
@@ -4,60 +4,67 @@ while generoReply.lower() != 'masculino' and generoReply.lower() != 'm' and gene
   generoReply = input('Digite um gênero correto. Opções disponiveis:\n Masculino - Homens\n M - Homens\n Feminino - Mulheres\n F - Mulheres\n Digite o gênero novamente: ')
 
 if generoReply.lower() == 'masculino' or generoReply.lower() == 'm':
-  reply = input('Digite a sua altura:\n')
-  
-  
-  while not reply.isdigit():
-   reply = input('apenas coloque números. Digite a sua altura novamente: ')
-   
-   if reply.isdigit():
-     break
+  try:
+   reply = float(input('Digite a sua altura:\n'))
+  except ValueError:
+    reply = float(input('não coloque frases. Apenas coloque números\nDigite a sua altura novamente: '))
+    
+  if str(reply)[1] != '.':
+    while not str(reply).isdigit():
+      if str(reply).replace('.', '').isdigit():
+       break
+    
+      reply = input('apenas coloque números. Digite a sua altura novamente: ')
    
   float(reply)
-  transform_string = str(reply)
-    
-  if transform_string[1] != '.':
+
+  if str(reply)[1] != '.':
    invalid = False
   
    while not invalid:
     reply = float(input('coloque a altura da maneira correta. Exemplo: 1.65\nDigite a sua altura: '))
     
-    string_reply = str(reply)
+    if str(reply)[1] == '.':
+      invalid = True
+      break
+
+  
+  calc = (72.7 * reply) - 58
+
+  print(f'O peso ideal para homens (baseado na sua altura) é {calc} KG')
+
+elif generoReply.lower() == 'feminino' or generoReply.lower() == 'f':
+  try:
+   alt = float(input('Digite a sua altura:\n'))
+  except ValueError:
+    alt = float(input('não coloque frases. Apenas coloque números\nDigite a sua altura novamente: '))
+  
+  
+  
+  if str(alt)[1] != '.':
+    while not str(alt).isdigit():
+      if str(alt).replace('.', '').isdigit():
+       break
+    
+      alt = input('apenas coloque números. Digite a sua altura novamente: ')
+   
+   
+  float(alt)
+  
+    
+  if str(alt)[1] != '.':
+   invalid = False
+  
+   while not invalid:
+    alt = float(input('coloque a altura da maneira correta. Exemplo: 1.65\nDigite a sua altura: '))
+    
+    string_reply = str(alt)
     
     if string_reply[1] == '.':
       invalid = True
       break
 
-  calc = (72.7 * reply) - 58
-
-  print(f'O peso ideal para homens (baseado na sua altura) é {round(calc)}')
-
-elif generoReply.lower() == 'feminino' or generoReply.lower() == 'f':
-  alt = input('Digite a sua altura:\n')
   
-  while not alt.isdigit():
-   alt = input('apenas coloque números. Digite a sua altura novamente: ')
-   
-   if alt.isdigit():
-     break
-   
-  float(alt)
-  
-  transform_str = str(alt)
-  
-  if transform_str[1] != '.':
-  
-   heigthInvalid = False
-  
-   while not heigthInvalid:
-    alt = float(input('coloque a altura da maneira correta. Exemplo: 1.65\nDigite a altura novamente: '))
-    
-    str_alt = str(alt)
-    
-    if str_alt[1] == '.':
-     invalid = True
-     break
-
   calc = (62.1 * alt) - 44.7
 
-  print(f'O peso ideal para mulheres (baseado na sua altura) é {round(calc)}')
+  print(f'O peso ideal para mulheres (baseado na sua altura) é {calc} KG')
--- read_txt.py
+++ read_txt.py
@@ -1,9 +1,11 @@
 import os.path
 
-if not os.path.isfile('file.txt'):
-    print('Criando arquivo...')
-    f = open('file.txt', 'a')
-    f.write('2 + 2')
+if not os.path.isfile('file.txt'):    
+    writeTxt = input('eu não encontrei o arquivo. Sendo assim, qual calculo você deseja no arquivo?\n')
+    
+    f = open('file.txt', 'a')    
+    f.write(writeTxt)
+    print('arquivo criado com o cálculo %s' % writeTxt)
 
 f = open('file.txt', 'r')
 
@@ -22,4 +24,4 @@ elif calc[1] == '*':
 elif calc[1] == '/':
     operator = int(calc[0]) / int(calc[2])
     
-print(operator)
+print(f'Resultado: {operator}')

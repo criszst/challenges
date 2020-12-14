9f8f783dee3832e4b529a61261807f60377f3c63
AdrianCristianSantos <adrian.cristian.st@gmail.com>
2020-12-14 18:53:03 -0300

    refatorado o "date.py"

--- date.py
+++ date.py
@@ -1,137 +1,66 @@
 # um pequeno "programa" que lê uma data fornecida pelo usuário (no formato dd/mm/yyyy) e, caso essa data for válida, ele imprime "data válida" no terminal, caso não for, ele imprime "data inválida".
 
-def validate(date):
-    str(date)
-    err = ''
-    
-    if len(date) < 8 or len(date) > 10:
-        while len(date) < 8 and len(date) > 10:
-         err = input('[data inválida] --- a data tem menos que 8 caracteres ou tem mais que 10 caracteres, se tornando, assim, uma data inválida. Exemplo de uma data correta para o sistema: 10/12/2020\n Insira uma data novamente: ')
-         
-         if len(err) == 8 or len(err) == 9 or len(err) == 10:
-             date = err
-             break
 
-        
-        while len(date) < 8 or len(date) > 10:
-         err = input('[data inválida] --- a data tem menos que 8 caracteres ou tem mais que 10 caracteres, se tornando, assim, uma data inválida. Exemplo de uma data correta para o sistema: 10/12/2020\n Insira uma data novamente: ')
-         
-         if len(str(err)) == 8 or len(str(err)) == 9 or len(str(err)) == 10:
-             date = err
-             break
-           
-    if not '/' in date:
-      while not '/' in date:
-        err = input('[date inválida] --- a data nao contém nenhuma barra (/).\nExemplo de uma data correta para o sistema: 1/2/2020.\nDigite uma data novamente:\n')
-        
-        if '/' in err:
-          date = err
-          break
-         
-         
-    if len(date) == 8:
-        
-      if not date.replace('/', '').isdigit():
-         while not date.replace('/', '').isdigit():
-          err = input('[data inválida] --- apenas coloque números na data.\nExemplo de uma data correta para o sistema: 1/1/2020\nInsira uma data novamente: ')
-         
-          if str(date).replace('/', '').isdigit():
-             date = err
-             break
-         
-         
-      if len(date[0]) == 1:
-          
-        if date[1] != '/':
-          while date[1] != '/':
-            err = input('[data inválida] --- a data não contém uma barra (/) nas posições corretas.\nExemplo de uma data correta para o sistema: 1/2/2020\nInsira uma data novamente: ')
-            
-            if str(err[1]) == '/':
-               date = err
-               break
-           
-         
-      elif len(date[0]) == 2:
-          
-        if date[2] != '/':
-         while date[2] != '/':
-           err = input('[data inválida] --- a data não contém uma barra (/) nas posições corretas.\nExemplo de uma data correta para o sistema: 10/1/2020\nInsira uma data novamente: ')
-            
-           if str(err[2]) == '/':
-              date = err
-              break
-          
-          
-    elif len(date) == 9:
-        
-     if not date.replace('/', '').isdigit():
-         while not date.replace('/', '').isdigit():
-          err = input('[data inválida] --- apenas coloque números na data.\nExemplo de uma data correta para o sistema: 10/12/2020\nInsira uma data novamente: ')
-         
-          if str(err).replace('/', '').isdigit():
-              date = err
-              break
-         
-         
-     if len(date[0]) == 2:
-         
-       if date[2] != '/':
-          while date[2] != '/':
-            err = input('[data inválida] --- a data não contém uma barra (/) nas posições corretas.\nExemplo de uma data correta para o sistema: 1/2/2020\nInsira uma data novamente: ')
-            
-            if str(err[2]) == '/':
-               date = err
-               break
-           
-           
-     elif len(date[0]) == 1:
-         
-       if date[2] != '/':
-          while date[2] != '/':
-            err = input('[data inválida] --- a data não contém uma barra (/) nas posições corretas.\nExemplo de uma data correta para o sistema: 10/2/2020\nInsira uma data novamente: ')
-            
-            if str(err[2]) == '/':
-               date = err
-               break
-             
-         
-    elif len(date) == 10:
-        
-     if not date.replace('/', '').isdigit():
-         while not date.replace('/', '').isdigit():
-          err = input('[data inválida] --- apenas coloque números na data.\nExemplo de uma data correta para o sistema: 10/12/2020\nInsira uma data novamente: ')
-         
-          if str(err).replace('/', '').isdigit():
-              date = err
-              break
-          
-     if len(date[0]) == 1:
-          
-        if date[1] != '/':
-          while date[1] != '/':
-            err = input('[data inválida] --- a data não contém uma barra (/) nas posições corretas.\nExemplo de uma data correta para o sistema: 1/2/2020\nInsira uma data novamente: ')
-            
-            if str(err[1]) == '/':
-               date = err
-               break
-           
-         
-     elif len(date[0]) == 2:
-          
-        if date[2] != '/':
-         while date[2] != '/':
-           err = input('[data inválida] --- a data não contém uma barra (/) nas posições corretas.\nExemplo de uma data correta para o sistema: 10/1/2020\nInsira uma data novamente: ')
-            
-           if str(err[2]) == '/':
-              date = err
-              break
-         
-        
-    return True
+def validate(date):
+   msg = 'data válida'
+  
+   if not date.replace('/', '').isdigit():
+     while not date.isdigit():
+       date = input('[data inválida] --- apenas coloque números. Coloque uma data no formato "dd/mm/yyyy" (exemplo: 15/7/2020).\nDigite uma data novamente: ')
+       
+       if date.replace('/', '').isdigit():
+         break
+       
+   if not '/' in date:
+     while not '/' in date:
+      date = input('[data inválida] --- a data não contém uma barra (/)\nExemplo de uma data correta para o sistema: 1/2/2020\nDigite uma data novamente: ')
+  
+      if '/' in date:
+        break
+  
+   day, month, year = map(int, date.split('/'))
+   
+   
+   months_31 = [1, 3, 5, 7, 8, 10, 12]
+   months_30 = [4, 6, 9, 11]
+   
+   leap_year = year % 4 == 0 and True or False
+   
+   if day == 0:
+     msg = f'[data inválida] --- o número "{day}" não é um dia válido.'
+   
+   
+   elif month > 12 or month <= 0:
+     msg = f'[data inválida] --- o número "{month}" não é um mês válido.'
+     
+     
+   elif year < 0000:
+     msg = f'[data inválida] --- o número "{year}" não é um ano válido.'
+     
+     
+   elif month == 2 and day >= 30:
+     msg = f'[data inválida] --- o mês "2" tem apenas 28 dias (exceto em anos bissextos).'
+   
+   
+   elif month in months_31 and day > 31:
+     msg = f'[data inválida] --- o mês "{month}" contém apenas 31 dias. Você colocou um dia que não existe no mês.'
+    
+    
+   elif month in months_30 and day > 30:
+    msg = f'[data inválida] --- o mês "{month}" contém apenas 30 dias. Você colocou um dia que não existe no mês.'
+    
+    
+   elif leap_year == True and month == 2 and day == 29:
+     msg = 'data válida'
+    
+    
+   elif leap_year == False and month == 2 and day == 29:
+     msg = f'[data inválida] --- o ano não é bissexto, logo, você não pode colocar o dia 29 no mês 2.'
+     
+
+   return print(msg)
+
 
 date = input('Digite uma data:\n')
 
-true_or_false = validate(date)
-
-if true_or_false == True:
-    print('data válida')
+validate(date)
--- read_txt.py
+++ read_txt.py
@@ -1,6 +1,7 @@
 # programa para ler os números de um arquivo.txt e calcular os mesmos de lá. Por exemplo, caso um arquivo.txt contenha o calculo "10 / 2", ele irá calcular o mesmo e irá imprimir o resultado no terminal.
 
 import os.path
+import sys
 
 if not os.path.isfile('file.txt'):    
     writeTxt = input('eu não encontrei o arquivo. Sendo assim, qual cálculo você deseja no arquivo?\n')
@@ -24,28 +25,32 @@ if not os.path.isfile('file.txt'):
 f = open('file.txt', 'r')
 
 read = f.read()
+      
 calc = read.split(' ')
-operator = ''
- 
-try:
- if calc[1] == '+':
+
+if len(calc) == 1:
+  print(f'não há cálculos para ser calculado. Texto no arquivo: {read}')
+  
+else:
+  
+  operator = ''
+
+
+  if calc[1] == '+':
    operator = int(calc[0]) + int(calc[2])
     
- elif calc[1] == '-':
+  elif calc[1] == '-':
    operator = int(calc[0]) - int(calc[2])
     
- elif calc[1] == '*':
+  elif calc[1] == '*':
     operator = int(calc[0]) * int(calc[2])
 
- elif calc[1] == '/':
+  elif calc[1] == '/':
     operator = int(calc[0]) / int(calc[2])
     
     
- if operator == '':
+  if operator == '':
     print(f'Sem resultado aparente. Texto no arquivo:\n{read}')
 
- else:
-  print(f'Resultado: {operator}')
-  
-except IndexError:    
-    print(f'não há calculos para ser calculado. Texto no arquivo: {read}')
+  else:
+   print(f'Resultado: {operator}')

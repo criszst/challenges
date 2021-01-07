cd4415345b30d484ff01f56b8aaf1ab7fb6c9346
AdrianCristianSantos <adrian.cristian.st@gmail.com>
2021-01-07 11:16:47 -0300

    método "find" foi adicionado na classe "user"

--- WikiPython/class/firstClass.py
+++ WikiPython/class/firstClass.py
@@ -1,16 +1,34 @@
 class user(object):
     users = []
-        
-    def __init__(self, nome, idade):
+
+    def __init__(self, nome, Id):
         self.nome = nome
-        self.idade = idade
-    
+        self.Id = Id
+
     def save(self):    
       self.__class__.users.append(self)
-    
+      
+    def find(self):
+      msg = ''
+      if self in self.__class__.users:
+        msg = print(f'\nUsuário encontrado:\n[Nome: {self.nome}, Id: {self.Id}]')
+      else:
+        msg = print('Usuário não encontrado.')
+        
+      return msg
+
     def __repr__(self):
-        return 'User: %s, Idade: %s' % (self.nome, self.idade)
-    
+        return 'User: %s, Id: %s' % (self.nome, self.Id)
+
     @classmethod
     def all(cls):
       return cls.users
+    
+xpto = user('XXXX', '99')
+
+a = user('aaaa', '222')
+
+xpto.save()
+a.save()
+
+print(user.find(a))
--- WikiPython/class/importClass.py
+++
@@ -1,6 +0,0 @@
-from WikiPython import user
-
-xpto = user('Adrian Cristian', '99')
-
-xpto.save()
-print(user.all())

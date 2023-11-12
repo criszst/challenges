44d3bc4ecf3815f41f538785cae24f4b8a5697ca
Cristian <56496903+Cristi4nSt@users.noreply.github.com>
2023-11-12 20:04:33 -0300

    Update check.py

--- WikiPython/decisionStructure/check.py
+++ WikiPython/decisionStructure/check.py
@@ -1,15 +1,7 @@
 # programa que verifica se um número é negativo ou se é positivo.
-
 try:
  number = int(input('Qual é o número para verificar se ele é negativo ou positivo?\n'))
 except ValueError:
  number = int(input('formato inválido. Digite um número novamente:\n'))
 
-reply = ''
-
-if number >= 0:
-    reply = 'número positivo'
-else:
-    reply = 'número negativo'
-    
-print(reply)
+print('positivo' if number >= 0 else 'negativo')
